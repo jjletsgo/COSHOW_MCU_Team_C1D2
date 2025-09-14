75abb0c2cb1b959af4ad4700e420aaae2106fb62
jjletsgo <dnrgus3914@knu.ac.kr>
2025-09-14 12:45:25 +0900

    checked everything working

--- main.c
+++ main.c
@@ -14,11 +14,12 @@
 
 
 
-
+//UART_INIT(), init_7_segment() 는 main에서 해주고 count_on_7_segment()랑 idle_7_segment() 는 버튼 입력에따라 메인 루프에서 호출해주면됩니다.
 
 int main(void) {
 	UART_INIT();
 	init_7_segment(); //타이머0 활성화 및 74595와 연결된 atmeag328p의 핀 설정
+	idle_7_segment(); //
 	count_on_7_segment(); //타이머0 오버플로 인터럽트 활성화 및 previous time을 millis()로 설정 및 cnt를 1로 설정
 	
 	while(1) {
