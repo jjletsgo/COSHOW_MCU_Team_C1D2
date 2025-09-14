19bfff828d40cc46017d61cc4e468aac5c67e124
formin21 <162601152+formin21@users.noreply.github.com>
2025-09-14 09:40:17 +0900

    Update display_ctrl.h

--- display_ctrl.h
+++ display_ctrl.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * display_ctrl.h
  *
  * Created: 2025-09-09 오전 11:04:15
@@ -22,25 +22,25 @@
 #define LCD_EN         (1<<2)
 #define LCD_BL         (1<<3)
 
-void i2c_init(void);
-void i2c_start(uint8_t addr7);
-void i2c_write(uint8_t data);
-void i2c_stop(void);
+static void i2c_init(void);
+static void i2c_start(uint8_t addr7);
+static void i2c_write(uint8_t data);
+static void i2c_stop(void);
 
-void lcd_set_ctrl(uint8_t data);
-void lcd_pulse_enable(uint8_t data);
-void lcd_write4(uint8_t nibble, uint8_t mode_rs);
-void lcd_send(uint8_t value, uint8_t mode_rs);
-void lcd_command(uint8_t cmd);
-void lcd_backlight_on(bool on);
-void lcd_write_char(uint8_t c);
-void lcd_clear(void);
-void lcd_set_cursor(uint8_t col, uint8_t row);
-void lcd_create_char(uint8_t location, const uint8_t pattern[8]);
-void lcd_init(void);
+static void lcd_set_ctrl(uint8_t data);
+static void lcd_pulse_enable(uint8_t data);
+static void lcd_write4(uint8_t nibble, uint8_t mode_rs);
+static void lcd_send(uint8_t value, uint8_t mode_rs);
+static void lcd_command(uint8_t cmd);
+static void lcd_backlight_on(bool on);
+static void lcd_write_char(uint8_t c);
+static void lcd_clear(void);
+static void lcd_set_cursor(uint8_t col, uint8_t row);
+static void lcd_create_char(uint8_t location, const uint8_t pattern[8]);
+static void lcd_init(void);
 
-void lcd_print_str(const char *s);
-void lcd_print_int(int32_t v);
-void lcd_print_float(float x);
+static void lcd_print_str(const char *str);
+static void lcd_print_int(uint16_t value);
+static void lcd_print_float(float value);
 
 #endif // DISPLAY_CTRL_H
