062a24741fd3af322c50133873a6a0983bf12e22
Minkyu <ria5296@naver.com>
2025-09-16 21:06:20 +0900

    Make develop main.c project
    Add "board.h" of global header using for any C files.
    And make a "main.c" file of develop version

---
+++ TreadMill/TreadMill/ADC.h
@@ -0,0 +1,11 @@
+﻿
+#ifndef ADC_H_
+#define ADC_H_
+
+void ADC_init(void);
+void ADC_select_channel(unsigned char channel);
+int read_ADC(void);
+
+uint16_t ADC_value;
+
+#endif /* ADC_H_ */
---
+++ TreadMill/TreadMill/motor_dc.c
@@ -0,0 +1,6 @@
+﻿/*
+ * motor_dc.c
+ *
+ * Created: 2025-09-16 오후 7:14:37
+ *  Author: sec
+ */ 
